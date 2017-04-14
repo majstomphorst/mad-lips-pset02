46eafc7e796c81d9eacc8d2ec0cf7089a8e7447f
majstomphorst <maxim.stomphorst@student.uva.nl>
2017-04-14 14:44:06 +0200

    Initial Commit

---
+++ mad lips - pset02/02-ViewController-UserInput.swift
@@ -0,0 +1,42 @@
+//
+//  02-ViewController-UserInput.swift
+//  mad lips - pset02
+//
+//  Created by Maxim Stomphorst on 11/04/2017.
+//  Copyright © 2017 M.a.j©. All rights reserved.
+//
+
+import UIKit
+
+class _2_ViewController_userinput: UIViewController {
+
+    
+    
+    // MARK: propertys
+    // making a list to store the users words in.
+    var words = [String]()
+    
+     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ mad lips - pset02/ViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  mad lips - pset02
+//
+//  Created by Maxim Stomphorst on 11/04/2017.
+//  Copyright © 2017 M.a.j©. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let path = "/Users/maj/Desktop/test.txt"
+        var text = ""
+        
+        do {
+            text = try String(contentsOfFile: path)
+            print(text)
+        } catch {
+            print("error")
+        }
+        
+        
+        let story = Story(stream: text)
+        var i = story.getPlaceholderCount()
+        print(i)
+        
+        for p in 1...i {
+            print(p)
+        }
+
+        
+        
+        story.fillInPlaceholder(word: "01")
+        print(story.isFilledIn())
+        story.fillInPlaceholder(word: "02")
+        print(story.isFilledIn())
+        i = story.getPlaceholderCount()
+        let data = story.toString()
+        
+        print(i)
+        print(data)
+        
+    
+        
+        
+    
+        
+        
+        
+      
+
+
+        
+//        var story = Story(stream: Hello")
+//        story.fillInPlaceholder(word: "")
+//        var text = story.toString()
+//        print(text)
+        
+        
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
